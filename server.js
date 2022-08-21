de0b111bdf102fa16aa9dd3634996d3d904ea203
Rodrigo <rodrigoperez0109@gmail.com>
2022-08-21 18:06:33 -0300

    h1

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/',(req, res) => {
-    res.send('Hello world with express')
+    res.send('<h1>Express</h1>')
 })
 
 app.listen(3000)
